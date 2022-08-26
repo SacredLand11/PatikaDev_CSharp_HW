e5d845ecc86f7c6730e954cf5bca73438724a06e
Meric AYDIN <101329548+SacredLand11@users.noreply.github.com>
2022-08-26 11:17:43 +0300

    ToDoApp

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :)");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("(1) Board Listelemek");
+            Console.WriteLine("(2) Board'a Kart Eklemek");
+            Console.WriteLine("(3) Board'dan Kart Silmek");
+            Console.WriteLine("(4) Kart Taşımak");
+
+            BroadManager operations = new BroadManager();
+
+            int selection = int.Parse(Console.ReadLine());
+
+            switch (selection)
+            {
+                case 1:
+                    operations.boardListeleme();
+                    break;
+                case 2:
+                    operations.kartEkle();
+                    break;
+                case 3:
+                    operations.kartSil();
+                    break;
+                case 4:
+                    operations.kartTasi();
+                    break;
+                default:
+                    Console.WriteLine("Hatalı seçim yaptınız.");
+                    break;
+
+
+            }
+        }
+    }
+}
