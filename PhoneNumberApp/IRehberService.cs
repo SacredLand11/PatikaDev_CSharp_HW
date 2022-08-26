ef5e0f1c3e40d665c503bec9844fc45ff0d9a546
Meric AYDIN <101329548+SacredLand11@users.noreply.github.com>
2022-08-26 11:08:16 +0300

    PhoneNumberApp

---
+++ PhoneNumberApp/IRehberService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumberApp
+{
+    interface IRehberService
+    {
+        void numaraEkle();
+        void numaraListele();
+        void numaraSil();
+        void numaraAra();
+        void numaraGuncelle();
+    }
+}
